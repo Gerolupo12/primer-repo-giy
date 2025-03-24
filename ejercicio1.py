30090345beca07a41f44e5dc0a036c4f3f71810c
Gerolupo12 <ramallogeronimo00@gmail.com>
2025-03-24 07:51:06 -0300

    Comentarios Agregados a Ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,6 @@
+## Se define variable nombre 
 nombre = "Ariel"
+## Se define variable apellido
 apellido = "Lopez"
+## Se imprime mensaje por consola
 print(f"Hola soy {nombre} {apellido} estudiante de programacion")

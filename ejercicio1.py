cc69c108bece462e47e080c1a53b1bd6ce3df791
Gerolupo12 <ramallogeronimo00@gmail.com>
2025-03-24 07:48:39 -0300

    Modificacion Ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,3 @@
-nombre = "Ariel"
-apellido = "Lopez"
+nombre = input("Ingrese su nombre:")
+apellido = input("Ingrese su apellido:")
 print(f"Hola soy {nombre} {apellido} estudiante de programacion")
